771aac006c74c2e98b01b86f36874af338853b3b
Joe Hilgard <jhilgard@asc.upenn.edu>
2015-12-10 17:02:51 -0500

    Some old code, some new code.

--- GM.R
+++ GM.R
@@ -4,7 +4,10 @@ library(readxl)
 library(dplyr)
 library(metafor)
 
-dat = read_excel("GM_2015-11-10.xlsx")
+#dat = read_excel("GM_2015-11-10.xlsx")
+dat = read_excel("edited_data.xlsx")
+# CRUDE APPROXIMATION: get rough sample size given se(z)
+dat$Sample.size = (1/dat$Std.Err)^2+3
 
 # Check out unique combinations of study, subgroup, outcome.specific, etc.
 # Trying to figure out which setting they used for studies with multiple outcomes.
@@ -17,7 +20,29 @@ dat %>%
   View
 
 dat %>% 
+  select(Study:Time.point, Outcome:media.type) %>% 
+  group_by(Study, Subgroup, Comparison, Outcome, Time.point, Outcome.type, design, media.type) %>% 
+  summarize("count" = n()) %>% 
+  View
+
+# check sum of approx. sample sizes
+temp = dat %>% 
+  filter(Outcome == "behavior",
+         media.type == "antisocial",
+         design == "experimental",
+         Outcome.type == "antisocial/negative")
+
+temp %>% 
+  select(Study:Data.format, r:checked.by, Sample.size) %>% 
+  View
+# Export for note-taking
+
+dat %>% 
   select(Study) %>% 
   distinct() %>% 
   write.table("study_notes.txt", sep="\t", row.names=F)
-  
+
+# at face value, no averaging or aggregating
+naive = rma(yi = Fisher.s.Z, sei = Std.Err, data = temp)
+funnel(naive)
+forest(naive)
